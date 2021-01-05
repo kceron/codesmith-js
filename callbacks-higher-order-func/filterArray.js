d17cb5256ef1cd2d399e0db92bfa745e59623605
kceron <karemiceron@gmail.com>
2021-01-05 18:17:06 -0500

    filterArray

--- callbacks-higher-order-func/filterArray.js
+++ callbacks-higher-order-func/filterArray.js
@@ -5,15 +5,26 @@ function filterArray(array, callback) {
     if (callback(array[i])) newArray.push(array[i]);
   }
   return newArray;
-}
-const arrOfNums = [1, 2, 3, 4, 5];
-function func1(num) {
-  // ADD CODE HERE
-}
-function func2(num) {
-  // ADD CODE HERE
-}
-  
+};
 
+const arrOfNums = [1, 2, 3, 4, 5];
+
+function func1(num) {
+    // ADD CODE HERE
+    if(num % 2 == 0){
+      return num
+    }else{
+      return undefined
+    }
+};
+
+function func2(num) {
+    // ADD CODE HERE
+    if(num % 2 !== 0){
+      return num
+    }else{
+      return undefined
+    }
+};
   // console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
   // console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
