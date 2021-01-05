83ba7db1df214980cad5bfa03209e56b84f81169
kceron <karemiceron@gmail.com>
2021-01-04 23:06:05 -0500

    addWaldo

--- addWaldo.js
+++ addWaldo.js
@@ -1,4 +1,4 @@
-// Create a function addWaldo that accepts an object with keys being first names and values being last names.
+// Create a function addWaldo that accepts an object with keys being first names and values being last names. For example addWaldo({'Luke': 'Skywalker', 'Harley': 'Quinn', 'Ryan': 'Reynolds'}) should add the key 'Waldo' with the value 'unknown' to the object and return the mutated object.
 // ADD CODE HERE 
 function addWaldo(obj){
   if(obj["Waldo"] === undefined){
