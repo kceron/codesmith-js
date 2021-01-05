562b5681be14cdc15198e4471ba32e93ab8adbe5
kceron <karemiceron@gmail.com>
2021-01-05 13:06:02 -0500

    pluralize

--- callbacks-higher-order-func/pluralize.js
+++ callbacks-higher-order-func/pluralize.js
@@ -1,10 +1,12 @@
 // Create a function pluralize that takes an array of strings as input and returns a new array with an "s" added to the end of each string in the input array. For example, if the string "carrot" is in the input array, it should become the string "carrots" in the output array.
-
 // The body of the pluralize function should employ a single for loop that pluralizes each string in the input array.
 // ADD CODE HERE
-function pluralize(){
-  
-}
-
+function pluralize(strArr){
+    let newArr = [];
+    for(let i=0; i<strArr.length; i++){
+      newArr.push(strArr[i] + 's')
+    }
+    return newArr
+  }
 // const animals = ["dog", "cat", "tree frog"];
 // console.log(pluralize(animals)); // should log: ["dogs", "cats", "tree frogs"]
