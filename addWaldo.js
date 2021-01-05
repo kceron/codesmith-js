8d5c031322e8076c5f0be94504de9e98297d44f8
kceron <karemiceron@gmail.com>
2021-01-04 23:03:50 -0500

    addWaldo

---
+++ addWaldo.js
@@ -0,0 +1,10 @@
+// Create a function addWaldo that accepts an object with keys being first names and values being last names.
+// ADD CODE HERE 
+function addWaldo(obj){
+  if(obj["Waldo"] === undefined){
+    obj["Waldo"] = 'unknown'
+  }
+  return obj
+}
+const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
+// console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }
