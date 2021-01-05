55a691347a32f2a2d723123a061f3fb6a115b51e
kceron <karemiceron@gmail.com>
2021-01-04 23:33:00 -0500

    function-expression

---
+++ function-expression.js
@@ -0,0 +1,7 @@
+// Convert the function named functionDeclaration to an anonymous function expression and assign it to a variable called myFunc.
+// MODIFY THE CODE BELOW 
+const myFunc = function() {
+  return "Hi there!";
+};
+
+// console.log(myFunc()) //should log: "Hi there!"
