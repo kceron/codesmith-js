394a10c5ced79575037a8fa513ea7b29ce47b20b
kceron <karemiceron@gmail.com>
2021-01-06 21:33:10 -0500

    objOfMatches

--- callbacks-higher-order-func/objOfMatches.js
+++ callbacks-higher-order-func/objOfMatches.js
@@ -1,7 +1,13 @@
 // Construct a function objOfMatches that accepts two arrays and a callback. objOfMatches will build an object and return it. To build the object, objOfMatches will test each element of the first array using the callback to see if the output matches the corresponding element (by index) of the second array. If there is a match, the element from the first array becomes a key in an object, and the element from the second array becomes the corresponding value.
 // ADD CODE HERE
-function objOfMatches(){
-  
+function objOfMatches(arr1, arr2, callback){
+    let obj = {};
+    for(let i=0; i<arr1.length; i++){
+      if(callback(arr1[i]) === arr2[i]){
+        obj[arr1[i]] = arr2[i]
+      }
+    };
+    return obj
 };
 // const arr1 = ['hi', 'howdy', 'bye', 'later', 'hello'];
 // const arr2 = ['HI', 'Howdy', 'BYE', 'later', 'HELLO'];
