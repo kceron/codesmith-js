67bd46d4bc7d90e15314502d31aa86cef7229d57
kceron <karemiceron@gmail.com>
2021-01-06 22:13:44 -0500

    arrToObj

--- callbacks-higher-order-func/arrToObj.js
+++ callbacks-higher-order-func/arrToObj.js
@@ -1,6 +1,11 @@
 // Add code to the function arrToObj in the place marked "ADD CODE HERE" in order to achieve the desired console logs. arrToObj should return an object that has elements from the passed-in array as keys, and the outputs from the callback (when those elements are passed in) as the corresponding values.
 function arrToObj(array, callback) {
     // ADD CODE HERE
+    let obj = {};
+    for(let i=0; i<array.length; i++){
+      obj[array[i]] = callback(array[i])
+    };
+    return obj
 };
   
 
