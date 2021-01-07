81fd9e5bc852dfeee89d89c3161f4b8dc0a5956f
kceron <karemiceron@gmail.com>
2021-01-07 08:43:55 -0500

    majority

--- callbacks-higher-order-func/majority.js
+++ callbacks-higher-order-func/majority.js
@@ -1,7 +1,20 @@
 // Create a function majority that accepts an array and a callback. The callback will return either true or false. majority will iterate through the array and perform the callback on each element until it can be determined if the majority of the return values from the callback are true. If the number of true returns is equal to the number of false returns, majority should return false.
 // ADD CODE HERE
 function majority(arr, callback){
-  
+    let trueValues = 0;
+    let falseValues = 0;
+    for(let i=0; i<arr.length; i++){
+      if(callback(arr[i])){
+        trueValues += 1
+      }else{
+        falseValues += 1
+      }
+    };
+    if(trueValues <= falseValues){
+      return false
+    }else{
+      return true
+    }
 };
 
 // const isOdd = function(num) { return num % 2 === 1; };
