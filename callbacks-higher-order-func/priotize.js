e95919d35880886a1cd49e55ea188301619cc6cb
kceron <karemiceron@gmail.com>
2021-01-07 08:46:33 -0500

    priotize

---
+++ callbacks-higher-order-func/priotize.js
@@ -0,0 +1,1 @@
+// Create a function prioritize that accepts an array and a callback. The callback will return either true or false. prioritize will iterate through the array and perform the callback on each element, and return a new array, where all the elements that yielded a return value of true come first in the array, and the rest of the elements come second.
