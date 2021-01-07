141f91932006aeac16ea76972c10927d94f7693d
kceron <karemiceron@gmail.com>
2021-01-06 22:59:41 -0500

    multiMap

--- callbacks-higher-order-func/multiMap.js
+++ callbacks-higher-order-func/multiMap.js
@@ -1,7 +1,17 @@
 // Construct a function multiMap that will accept two arrays - an array of values and an array of callbacks. multiMap will return an object whose keys match the elements in the array of values. The corresponding values that are assigned to the keys will be arrays consisting of outputs from the array of callbacks, where the input to each callback is the key.
 // ADD CODE HERE
 function multiMap(arrOfVal, arrOfCallbacks){
-  
+    let resultObj = {};
+    for(let i=0; i<arrOfVal.length; i++){
+      let objValues = [];
+        // let vals = arrOfCallbacks.forEach(callback => callback(arrOfVal[i]))
+      for(let ii=0; ii<arrOfCallbacks.length; ii++){
+        let callback = arrOfCallbacks[i]
+        objValues.push(callback(arrOfVal[i]))
+      }
+          resultObj[arrOfVal[i]] = objValues
+    };
+    return resultObj
 };
 
 // function uppercaser(str) { return str.toUpperCase(); }
