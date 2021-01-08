4e77df5556a5ffa0fcfc5c33afbb6910951b9d66
kceron <karemiceron@gmail.com>
2021-01-07 19:48:58 -0500

    goodKeys

--- callbacks-higher-order-func/goodKeys.js
+++ callbacks-higher-order-func/goodKeys.js
@@ -1,6 +1,9 @@
 // Create a function goodKeys that accepts an object and a callback. The callback will return either true or false. goodKeys will iterate through the object and perform the callback on each value. goodKeys will then return an array consisting only the keys whose associated values yielded a true return value from the callback.
 // ADD CODE HERE
-
+function goodKeys(obj, callback){
+    let trueKeysArr = []
+    
+};
 
 // const sunny = { mac: 'priest', dennis: 'calculator', charlie: 'birdlaw', dee: 'bird', frank: 'warthog' };
 // function startsWithBird(str) { return str.slice(0, 4).toLowerCase() === 'bird'; };
