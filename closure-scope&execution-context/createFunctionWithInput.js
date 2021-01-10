9946d72ae3717000916de76b5d73d282910fa529
kceron <karemiceron@gmail.com>
2021-01-09 22:42:46 -0500

    createFunctionWithInput

---
+++ closure-scope&execution-context/createFunctionWithInput.js
@@ -0,0 +1,8 @@
+// Create a function createFunctionWithInput that accepts one input and returns a function. When that created function is called, it should return the input that was passed to createFunctionWithInput when it was created.
+// ADD CODE HERE
+
+
+// const sampleFunc = createFunctionWithInput('sample');
+// console.log(sampleFunc()); // should log: 'sample'
+// const helloFunc = createFunctionWithInput('hello');
+// console.log(helloFunc()); // should log: 'hello' 
