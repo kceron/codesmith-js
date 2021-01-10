c55786e2ad4cb8b9cfce92e0e3bcbbb03965dcf8
kceron <karemiceron@gmail.com>
2021-01-09 22:38:03 -0500

    createFunction

--- closure-scope&execution-context/createFunction.js
+++ closure-scope&execution-context/createFunction.js
@@ -1,7 +1,10 @@
 // Create a function createFunction that creates and returns a function. When that newly created function is called, it should return the string 'hello world'.
 // ADD CODE HERE
 function createFunction(){
-
+  function newlyCreatedFunc(){
+    return 'hello world'
+  }
+  return newlyCreatedFunc;
 };
 
 // const myFunction = createFunction();
