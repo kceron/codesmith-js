6d90027bd3ec63348517429fd1137ebc0c57d62d
kceron <karemiceron@gmail.com>
2021-01-11 08:25:21 -0500

    delay

--- closure-scope&execution-context/delay.js
+++ closure-scope&execution-context/delay.js
@@ -1,6 +1,10 @@
 // Write a function delay that accepts two arguments, a callback and the wait time in milliseconds. Delay should return a function that, when invoked waits for the specified amount of time before executing. HINT - research setTimeout();
 // ADD CODE HERE
-
+function delay(callback, waitTime){
+  return function() {
+  	setTimeout(callback, waitTime);
+	};
+};
 
 // let count = 0;
 // const delayedFunc = delay(() => count++, 1000);
