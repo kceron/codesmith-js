853da70ac0379e646e6873df89b09d70520c94c6
kceron <karemiceron@gmail.com>
2021-01-11 07:52:37 -0500

    after

--- closure-scope&execution-context/after.js
+++ closure-scope&execution-context/after.js
@@ -1,5 +1,17 @@
 // Write a function after that takes the number of times the callback needs to be called before being executed as the first parameter and the callback as the second parameter.
 // ADD CODE HERE
+function after(num, callback){
+  let count = 0
+  function canExecute(string){
+    count++
+  	if(count >= num){
+    	return callback(string)
+  	}else if(count < num){
+    	return
+  	};
+	};
+  return canExecute;
+};
 
 const called = function(string) { return('hello ' + string); };
 const afterCalled = after(3, called);
