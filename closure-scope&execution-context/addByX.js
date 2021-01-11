8a1aee858b6f470b479e606a7055afc7954eef10
kceron <karemiceron@gmail.com>
2021-01-11 08:34:06 -0500

    addByX

--- closure-scope&execution-context/addByX.js
+++ closure-scope&execution-context/addByX.js
@@ -1,9 +1,8 @@
 // Now we are going to create a function addByX that returns a function that will add an input by x.
 // ADD CODE HERE
 function addByX(input){
-  let x = input
   function addInput(num){
-    return x + num;
+    return input + num;
   }
   return addInput;
 };
