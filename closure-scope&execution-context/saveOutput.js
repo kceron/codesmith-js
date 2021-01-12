52558d371a882176ca786b77bf8f831a17bc2bf3
kceron <karemiceron@gmail.com>
2021-01-11 19:16:27 -0500

    saveOutput

--- closure-scope&execution-context/saveOutput.js
+++ closure-scope&execution-context/saveOutput.js
@@ -1,6 +1,15 @@
 // Create a function saveOutput that accepts a function (that will accept one argument), and a string (that will act as a password). saveOutput will then return a function that behaves exactly like the passed-in function, except for when the password string is passed in as an argument. When this happens, the returned function will return an object with all previously passed-in arguments as keys, and the corresponding outputs as values
 // ADD CODE HERE
-
+function saveOutput(func, string){
+  let resultObj = {};
+  return function(password){
+    if(password === string){
+      return resultObj;
+    }else{
+      return resultObj[password] = func(password)
+    }
+  };
+};
 
 // const multiplyBy2 = function(num) { return num * 2; };
 // const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
