866d633136e85983a9e17eece89c913234456cfe
kceron <karemiceron@gmail.com>
2021-01-11 20:29:38 -0500

    defineFirstArg

---
+++ closure-scope&execution-context/dateStamp.js
@@ -0,0 +1,14 @@
+// Create a function dateStamp that accepts a function and returns a function. The returned function will accept whatever arguments the passed-in function accepts and return an object with a date key whose value is today's date (not including the time) represented as a human-readable string (see the Date object for conversion methods), and an output key that contains the result from invoking the passed-in function.
+// ADD CODE HERE
+function dateStamp(func){
+  let resultObj = {};
+  return function(...args){
+    resultObj.date = new Date().toString().split(' ').splice(0,4).join(' ')
+    resultObj.output = func(...args)
+    return resultObj;
+  };
+};
+
+// const stampedMultBy2 = dateStamp(n => n * 2);
+// console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
+// console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
