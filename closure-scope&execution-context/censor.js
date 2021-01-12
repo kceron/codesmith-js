a0f817c84e5f1cc6187b68382a2d2f13343b11f4
kceron <karemiceron@gmail.com>
2021-01-11 21:07:29 -0500

    censor

--- closure-scope&execution-context/censor.js
+++ closure-scope&execution-context/censor.js
@@ -1,6 +1,18 @@
 // Create a function censor that accepts no arguments. censor will return a function that will accept either two strings, or one string. When two strings are given, the returned function will hold onto the two strings as a pair, for future use. When one string is given, the returned function will return the same string, except all instances of a first string (of a saved pair) will be replaced with the second string (of a saved pair).
 // ADD CODE HERE
-
+function censor(){
+  let storedStrings = {}
+  return function(strOne, strTwo){
+    if(strTwo){
+      storedStrings[strOne] = strTwo
+      return;
+    }
+    Object.keys(storedStrings).forEach(key => {
+      strOne = strOne.replace(key, storedStrings[key])
+    })
+		return strOne;
+  };
+};
 
 // const changeScene = censor();
 // changeScene('dogs', 'cats');
