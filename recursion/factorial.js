6c7690574e1aaba3e5f7097edf6790541b3a073c
kceron <karemiceron@gmail.com>
2021-01-13 08:43:24 -0500

    repeater

---
+++ recursion/factorial.js
@@ -0,0 +1,10 @@
+// Write a function that returns the factorial of a number.
+// EXAMPLE4! = 4 * 3 * 2 * 1 = 24, so calling factorial(4) should return 24.
+function factorial(num) {
+
+}
+
+
+// console.log(factorial(4)); // -> 24
+// console.log(factorial(6)); // -> 720
+// console.log(factorial(0)); // -> 1
--- recursion/repeater.js
+++ recursion/repeater.js
@@ -1,10 +1,8 @@
 // Write a function that takes an input character and returns that character repeated 5 times using recursion. For example, if the input is 'g', then the output should be 'ggggg'.
-console.log('Hello, world!');
-let result = "";
 function repeater(char) {
-	if (result.length === 5) { return result }
-  result += char;
-  return repeater(char);
+	if (char.length === 5) return char;
+  return repeater(char + char[0]);
 };
 
 // console.log(repeater('g'));
+// console.log(repeater('j'));
