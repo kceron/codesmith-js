85d11a08187cf0dea89de93c5ad2ec751b6842db
kceron <karemiceron@gmail.com>
2021-01-13 13:31:21 -0500

    factorial

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -1,9 +1,9 @@
 // Write a function that returns the factorial of a number.
 // EXAMPLE4! = 4 * 3 * 2 * 1 = 24, so calling factorial(4) should return 24.
 function factorial(num) {
-
-}
-
+	if (num === 0) return 1;
+  return num * factorial(num-1)
+};
 
 // console.log(factorial(4)); // -> 24
 // console.log(factorial(6)); // -> 720
