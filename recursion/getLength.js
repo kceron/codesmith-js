c6def5d504c3fb84a2ec64d05d77e9ab437964c3
kceron <karemiceron@gmail.com>
2021-01-13 13:33:36 -0500

    getLength

---
+++ recursion/getLength.js
@@ -0,0 +1,9 @@
+// Get the length of an array using recursion without accessing its length property.
+function getLength(array) {
+
+}
+
+// console.log(getLength([1])); // -> 1
+// console.log(getLength([1, 2])); // -> 2
+// console.log(getLength([1, 2, 3, 4, 5])); // -> 5
+// console.log(getLength([])); // -> 0
