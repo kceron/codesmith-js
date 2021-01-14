27fd1b5e7dea3ca20d01dc74d2970f63a33c34da
kceron <karemiceron@gmail.com>
2021-01-13 19:23:18 -0500

    repeater

--- recursion/repeater.js
+++ recursion/repeater.js
@@ -1,6 +1,6 @@
 // Write a function that takes an input character and returns that character repeated 5 times using recursion. For example, if the input is 'g', then the output should be 'ggggg'.
 function repeater(char) {
-	if (char.length === 5) return char;
+  if (char.length === 5) return char;
   return repeater(char + char[0]);
 };
 
