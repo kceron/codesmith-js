5e78ec40797b05b21ec0ef5d6e4c1d2cff5913b1
kceron <karemiceron@gmail.com>
2021-01-14 18:14:33 -0500

    setTimeout

---
+++ asynchronous-js/setTimeout.js
@@ -0,0 +1,4 @@
+// Write code that will log to the console, 'I am at the beginning of the code'.
+// Beneath that console log, set a timer (see setTimeout) that will log to the console 'I am in the setTimeout callback function' after 600 ms.
+// Next, add code to the end of the challenge to log 'I am at the end of the code'. Now re-run the code.
+// Add code here
