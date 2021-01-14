a88afa9be8e96dc194ed3422edda6825ead7a13c
kceron <karemiceron@gmail.com>
2021-01-13 21:26:46 -0500

    flow

---
+++ recursion/flow.js
@@ -0,0 +1,11 @@
+// Write a function that takes an array of functions and a number that will be piped through all those functions. The input number passes through the first function, whose output is passed as input to the second function, whose output is passed as input to the third function, and so on. Use recursion to return the final output of the last function in the array.
+function flow(input, funcArray) {
+
+};
+
+// function multiplyBy2(num) { return num * 2; }
+// function add7(num) { return num + 7; }
+// function modulo4(num) { return num % 4; }
+// function subtract10(num) { return num - 10; }
+// const arrayOfFunctions = [multiplyBy2, add7, modulo4, subtract10];
+// console.log(flow(2, arrayOfFunctions)); // -> -7
