9b13df16042af7308cd8e987e2e059333e7ff001
kceron <karemiceron@gmail.com>
2021-01-15 11:52:07 -0500

    forEach

--- asynchronous-js/promises.js
+++ asynchronous-js/promises.js
@@ -1,12 +1,9 @@
 // Challenge 1
 // Let's start by reviewing asynchronous functions! Using setTimeout, print the string 'Hello!' after 1000ms.
 // Challenge 1
-
 function sayHello() {
-
+  setTimeout(function(){ console.log('Hello'); }, 1000);
 }
-
-// Uncomment the line below when ready
 // sayHello(); // should log "Hello" after 1000ms
 
 
