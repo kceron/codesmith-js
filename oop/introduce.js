9b54af40597e93d2fae8d09b0ce1f4a51c87bf21
kceron <karemiceron@gmail.com>
2021-01-17 15:55:53 -0500

    introduce

---
+++ oop/introduce.js
@@ -0,0 +1,20 @@
+// Without editing the code you've already written, add an introduce method to the personStore object that logs "Hi, my name is [name]".
+const personStore = {
+  greet: function() {
+    console.log('hello');
+  }
+}
+
+function personFromPersonStore(name, age) {
+	const person = Object.create(personStore);
+  person.name = name;
+  person.age = age;
+  return person;
+}
+
+const sandra = personFromPersonStore('Sandra', 26);
+
+// add code here
+
+
+// sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
