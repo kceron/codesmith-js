843b515e728c2def00618088d5523051e7db92c3
kceron <karemiceron@gmail.com>
2021-01-17 12:46:13 -0500

    limitedInterval

--- asynchronous-js/limitedInterval.js
+++ asynchronous-js/limitedInterval.js
@@ -5,6 +5,10 @@
 // limit time in milliseconds.
 // limitedInterval should run the callback once every wait milliseconds, up to limit milliseconds, and then stop.
 // Add code here
+function limitedInterval(cb, wait, limit){
+  for(let i=0; i<limit-wait; i+=wait){
+    setTimeout(cb, wait);
+  };
+};
 
-// /* Uncomment the following line and click 'run' to test your work */
 // limitedInterval(() => console.log('repeating'), 100, 550); // should log 'repeating' once per 100 ms, five times.
