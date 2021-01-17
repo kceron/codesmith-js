648838bf546e5fce21e221d8a5ecb730438e3710
kceron <karemiceron@gmail.com>
2021-01-17 15:38:30 -0500

    personStore

--- oop/personStore.js
+++ oop/personStore.js
@@ -1,8 +1,9 @@
 // Inside personStore object, create a property greet where the value is a function that logs "hello".
 const personStore = {
 	// add code here
-
-
+  greet: function() {
+    console.log('hello')
+  }
 };
 
 // personStore.greet(); // -> Logs 'hello'
