321693f8ee8c14a22500a2108db6c087217066f4
kceron <karemiceron@gmail.com>
2021-01-18 11:12:45 -0500

    personConstructor

--- oop/personConstructor.js
+++ oop/personConstructor.js
@@ -1,9 +1,8 @@
 // Create a function PersonConstructor that uses the this keyword to save a single property onto its scope called greet. greet should be a function that logs the string "hello".
 function PersonConstructor() {
 	// add code here
-
-
-}
+  this.greet = function(){ console.log('hello') };
+};
 
 const simon = new PersonConstructor();
 
