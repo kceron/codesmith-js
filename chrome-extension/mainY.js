443a7b8c6e294a307f31a1bfd2271bb0637f262f
kceron <karemiceron@gmail.com>
2021-01-23 19:40:43 -0500

    css

--- chrome-extension/mainY.js
+++ chrome-extension/mainY.js
@@ -1,33 +1,47 @@
 // youtube
-const images = document.querySelectorAll("img");
-const parentArr = []
-images.forEach(function(img) {
-  parentElement = img.parentElement
-  parentArr.push(parentElement)
-});
-images.forEach(function(img) {
-  img.remove()
-});
+const contents = document.getElementById('contents');
+parentElement = contents.parentElement
+contents.parentNode.removeChild(contents);
 
-parentArr.forEach(parent => {
-  const img = document.createElement("img");
-  img.src = "https://i.pinimg.com/564x/f0/3c/6d/f03c6d0bd32c465caf05e90361a5e3e4.jpg"
-  parent.appendChild(img);
-});
+const h1 = document.createElement("h1")
+h1.textContent += 'Get back to achieving your coding dreams';
+h1.classList.add("beautText");
+parentElement.appendChild(h1)
 
-// const contents = document.getElementById('contents');
-// parentElement = contents.parentElement
-// contents.parentNode.removeChild(contents);
-// const img = document.createElement("img");
-// img.src = "https://www.trinityp3.com/wp-content/uploads/2019/04/strong-brand.jpeg"
-// parentElement.appendChild(img); 
+const img = document.createElement("img");
+img.src = "https://www.trinityp3.com/wp-content/uploads/2019/04/strong-brand.jpeg"
+parentElement.appendChild(img); 
 
-
-// jQuery
-// var contents = $('contents');
-// $( document ).ready(function() {
-//   // Handler for .ready() called.
-//   contents.remove(contents);
+// const images = document.querySelectorAll("img");
+// const parentArr = []
+// images.forEach(function(img) {
+//   parentElement = img.parentElement
+//   parentArr.push(parentElement)
 // });
-// var contents = $('#contents');
-// contents.remove();
+
+// images.forEach(function(img) {
+//   img.remove()
+// });
+
+// parentArr.forEach(parent => {
+//   const img = document.createElement("img");
+//   img.src = "https://i.pinimg.com/564x/f0/3c/6d/f03c6d0bd32c465caf05e90361a5e3e4.jpg"
+//   parent.appendChild(img);
+// });
+
+// const titles = document.querySelectorAll("details");
+// const titlesArr = []
+// titles.forEach(function(title) {
+//   parentElement = title.parentElement
+//   titlesArr.push(parentElement)
+// });
+
+// titles.forEach(function(title) {
+//   title.remove()
+// });
+
+// titlesArr.forEach(parent => {
+//   const h3 = document.createElement("h3");
+//   h3.textContent += 'Get back to achieving your coding dreams';
+//   parent.appendChild(h3)
+// });
