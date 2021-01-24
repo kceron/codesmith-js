46ec76efbcc01bc32e8e53c76a4d095d27995dd2
kceron <karemiceron@gmail.com>
2021-01-23 20:17:11 -0500

    added css to all files

--- chrome-extension/mainF.js
+++ chrome-extension/mainF.js
@@ -1,3 +1,13 @@
 // facebook
 const pageContent = document.getElementById("mount_0_0"); 
+parentElement = pageContent.parentElement 
 pageContent.parentNode.removeChild(pageContent);
+
+const h1 = document.createElement("h1")
+h1.textContent += 'Get back to achieving your coding dreams';
+h1.classList.add("beautText");
+parentElement.appendChild(h1)
+
+const img = document.createElement("img");
+img.src = "https://www.trinityp3.com/wp-content/uploads/2019/04/strong-brand.jpeg"
+parentElement.appendChild(img); 
--- chrome-extension/mainR.js
+++ chrome-extension/mainR.js
@@ -1,6 +1,22 @@
 // reddit
-const pageCont = document.getElementById("2x-container"); 
-// const pageCont = $("2x-container"); 
+const pageCont = document.getElementById("TrendingPostsContainer");
+parentElement = pageCont.parentElement 
 pageCont.parentNode.removeChild(pageCont);
+
+const h1 = document.createElement("h1")
+h1.textContent += 'Get back to achieving your coding dreams';
+h1.classList.add("beautText");
+parentElement.appendChild(h1)
+
+const img = document.createElement("img");
+img.src = "https://www.trinityp3.com/wp-content/uploads/2019/04/strong-brand.jpeg"
+parentElement.appendChild(img); 
+
+
+
+
+
+
+
 
 // var contents = document.getElementsByClassName('.someClass');
